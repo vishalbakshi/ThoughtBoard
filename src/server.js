c61caddf659eb1ea314bfede7642aefd55857409
vishalbakshi <vdbakshi@gmail.com>
2019-01-07 20:53:32 -0800

    add index.pug and functionality to server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,32 @@
 const express = require("express");
-
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.route("/").get(function(req, res) {
-  res.send("Hello!");
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", express.static("views/index.html"));
+
+app.set("view engine", "pug");
+
+let thoughts = {};
+
+app
+  .route("/")
+  .get(function(req, res) {
+    console.log("GET received");
+    res.render("index", { thoughts: thoughts });
+  })
+  .post(function(req, res) {
+    console.log(req.body);
+
+    if (!thoughts[req.body["category"]]) {
+      thoughts[req.body["category"]] = [req.body["thought"]];
+    } else {
+      thoughts[req.body["category"]].push(req.body["thought"]);
+    }
+    console.log(thoughts);
+    res.redirect("/");
+  });
 
 app.listen(8080, function() {
   console.log("Listening on 8080");
