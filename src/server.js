ec9f76fac2aa8e798d2856275b86a141a17b6f50
Vishal Bakshi <vdbakshi@gmail.com>
2019-01-13 22:26:49 -0800

    Create index.html

--- src/server.js
+++ src/server.js
@@ -11,9 +11,22 @@ app.set("view engine", "pug");
 app
   .route("/")
   .get(function(req, res) {
-    res.send("Hello World!");
+    res.sendFile("/sandbox/views/index.html");
   })
-  .post(function(req, res) {});
+  .post(function(req, res) {
+    let formData = {};
+    Object.keys(req.body).forEach(function(key) {
+      if (!formData[req.body[key]]) {
+        formData[req.body[key]] = [key];
+      } else {
+        formData[req.body[key]].push(key);
+      }
+    });
+
+    Object.value;
+    console.log(formData);
+    res.redirect("/");
+  });
 
 app.listen(8080, function() {
   console.log("Listening on 8080");
