01b7eff46884ffbcb0c5c153363b68227014f886
Vishal Bakshi <vdbakshi@gmail.com>
2019-01-17 08:20:47 -0800

    Create index.pug

--- src/server.js
+++ src/server.js
@@ -7,25 +7,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", express.static("views/index.html"));
 
 app.set("view engine", "pug");
+let thoughts = {
+  thought1_id: "Thought 1",
+  thought2_id: "Thought 2",
+  thought3_id: "Thought 3",
+  thought4_id: "Thought 4"
+};
 
+let formData = {};
 app
   .route("/")
   .get(function(req, res) {
-    res.sendFile("/sandbox/views/index.html");
+    //res.sendFile("/sandbox/views/index.html");
+    res.render("index", { formData: formData });
   })
   .post(function(req, res) {
-    let formData = {};
     Object.keys(req.body).forEach(function(key) {
-      if (!formData[req.body[key]]) {
-        formData[req.body[key]] = [key];
+      if (formData[req.body[key]] === undefined) {
+        let newObj = {};
+        newObj[key] = thoughts[key];
+        formData[req.body[key]][key] = thoughts[key];
       } else {
-        formData[req.body[key]].push(key);
+        if (formData[req.body[key]][key] === undefined) {
+          formData[req.body[key]][key] = thoughts[key];
+        }
       }
     });
-
-    Object.value;
     console.log(formData);
-    res.redirect("/");
+    //res.redirect("/");
+    res.render("index", { formData: formData });
   });
 
 app.listen(8080, function() {
