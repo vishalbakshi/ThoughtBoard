d1c06f7defa3de5871a4f74439f6a025fa916415
Vishal Bakshi <vdbakshi@gmail.com>
2019-01-17 13:37:43 -0800

    Create index2.pug

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", express.static("views/index.html"));
 
 app.set("view engine", "pug");
+
 let thoughts = {
   thought1_id: "Thought 1",
   thought2_id: "Thought 2",
@@ -15,6 +16,7 @@ let thoughts = {
 };
 
 var categories = {
+  no_category: "No Category",
   category1: "Category 1",
   category2: "Category 2",
   category3: "Category 3"
@@ -24,14 +26,12 @@ let formData = {};
 app
   .route("/")
   .get(function(req, res) {
-    console.log(categories);
     //res.sendFile("/sandbox/views/index.html");
     res.render("index", { formData: formData, categories: categories });
   })
   .post(function(req, res) {
     let formData = {};
     Object.keys(req.body).forEach(function(key) {
-      console.log(req.body[key]);
       if (formData[req.body[key]] === undefined) {
         let newObj = {};
         newObj[key] = thoughts[key];
@@ -42,7 +42,7 @@ app
     });
     console.log(formData);
     //res.redirect("/");
-    res.render("index", { formData: formData, categories: categories });
+    res.render("index2", { formData: formData, categories: categories });
   });
 
 app.listen(8080, function() {
