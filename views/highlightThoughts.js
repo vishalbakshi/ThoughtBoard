74acd3bee8f9850c1328dc4c2424aedba4dc297c
Vishal Bakshi <vdbakshi@gmail.com>
2019-01-29 16:30:07 -0800

    update files to accomodate highlightThoughts.js tests

--- views/highlightThoughts.js
+++ views/highlightThoughts.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-  /*
   $("#highlightByTag").click(function(e) {
-    e.preventDefault();
-    let tagClass = "." + $("#selectTag").val();
-    $(tagClass).addClass("selected");
+    let tag = $("select[name=new_tag]").val();
+
+    $(".thought").each(function(idx, thought) {
+      if ($(this).has("span[name=" + tag + "]").length) {
+        $(this).addClass("selected");
+      }
+      e.preventDefault();
+      return false;
+    });
   });
-  */
 });
--- views/tests.js
+++ views/tests.js
@@ -1,8 +1,12 @@
-QUnit.test("User can assign a tag to a thought", function(assert) {
-  $("input[class=thoughtCheckbox]").trigger("click");
-  $("select[name=new_tag]").val($(".thought > span").attr("class"));
-});
 QUnit.test("User can highlight thoughts by tag", function(assert) {
   $("input[class=thoughtCheckbox]").trigger("click");
-  $("select[name=new_tag]").val($(".thought > span").attr("class"));
+  $("select[name=new_tag]").val("0");
+  let originalThoughtColor = $(".thought").css("background-color");
+  $("input[name=highlight]").trigger("click");
+
+  assert.notEqual(
+    $(".thought").css("background-color"),
+    originalThoughtColor,
+    "Thought div with tag changes color upon clicking `Highlight Thoughts` after tag is selected"
+  );
 });
